f3e3e72bd35fc3e8bd1736ca22cd2363927c5a30
Madhav <madhavk@cse.iitb.ac.in>
2025-08-31 21:59:06 +0530

    Task 2

---
+++ Project/my-next-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Welcome to Next.js!</h2>
+      </main>
+    </div>
+  );
+}
---
+++ Project/my-next-app/components/Header.tsx
@@ -0,0 +1,8 @@
+// components/Header.tsx
+export default function Header() {
+  return (
+    <header style={{ padding: '10px', backgroundColor: '#f3f3f3' }}>
+      <h1>My Next.js App</h1>
+    </header>
+  );
+}
